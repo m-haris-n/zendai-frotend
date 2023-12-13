a27fd044b8e9f56df4e3ce1dda6a83cfe5186226
mharisn <m.haris.nabeel@gmail.com>
2023-12-13 21:48:40 +0500

    minor fix

--- src/components/dashboard/ChatArea.tsx
+++ src/components/dashboard/ChatArea.tsx
@@ -109,7 +109,7 @@ export default function ChatArea() {
         />
         <Button
           size={'lg'}
-          onClick={handleSendMsg}
+          onClick={() => handleSendMsg}
 
           // disabled={!isZenned}
         >
